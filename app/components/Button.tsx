ad709646fd2403e2934e0584b75c13b8866c2a73
kreztofer <amalu.uchenna.7@gmail.com>
2026-05-12 10:22:02 +0100

    tv

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -8,19 +10,36 @@ const Button: React.FC<ButtonProps> = ({
   children,
   variant = "default",
   className,
+  onClick,
   ...props
 }) => {
   const base =
     "px-7 py-2.5 cursor-pointer hover:scale-105 rounded-[10px] text-[14px] transition-all duration-300 ease-in-out transform";
 
   const variants: Record<string, string> = {
-    default: "bg-white text-black ",
+    default: "bg-white text-black",
+    outline: "border border-gray-400 bg-transparent cursor-pointer",
+  };
 
-    outline: "border border-gray-400 bg-transparent cursor-pointer ",
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    onClick?.(e);
+
+    if (variant === "default") {
+      const link = document.createElement("a");
+      link.href = "/hotelmantv.apk";
+      link.download = "hotelmantv.apk";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
   };
 
   return (
-    <button className={`${base} ${variants[variant]}`} {...props}>
+    <button
+      className={`${base} ${variants[variant]} ${className || ""}`}
+      onClick={handleClick}
+      {...props}
+    >
       {children}
     </button>
   );
